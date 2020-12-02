71a2269b10ecd5e2284d9bfd6496cbfe357689f0
Muhammetkaya3478 <66886314+Muhammetkaya3478@users.noreply.github.com>
2020-12-02 13:45:54 +0300

    C
    Hello. I am Muhammet. I am a Computer Engineering student. I am in 1st grade. Here I will share with you my c projects that I am learning.

---
+++ Project-1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main(int argc, char** argv) {
+	
+	printf("****************\n\n");
+	
+	printf("Student:Muhammet Kaya\n");
+	printf("Class: 1 \n");
+	printf("Department:Computer Engineering \n\n");
+	
+	printf("****************");
+	
+	
+	// \n 
+	
+	return 0;
+}
